5a4ad476d7c72cdbe3c0298fe9cb4e0ed8022ec8
Peter Stjern <peter@stjern.se>
2018-04-24 20:03:31 +0200

    Begun building the app

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+import Search from './models/Search';
+
+/* GLOBAL STATE OF THE APP
+* - Search Object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes 
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' //TODO
+
+    if(query){
+        // 2) new search object and add to state
+        state.search = new Search(query);
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResult();
+
+        // 5) Render results on UI
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+    
+    async getResult(){
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'b16909b2e4d8ad90b30ac4552c8bcd79';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes
+            // console.log(this.result);
+    
+        } catch(error) {
+            alert(error);
+        }
+    }
+}
+
+
+//Api Key food2fork b16909b2e4d8ad90b30ac4552c8bcd79
+//All search requests should be made to the base search API URL. http://food2fork.com/api/search 
+//All recipe requests should be made to this URL: http://food2fork.com/api/get 
+
